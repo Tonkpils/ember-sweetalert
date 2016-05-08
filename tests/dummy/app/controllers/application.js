5c1fa9ba4e012a25aea47d5f08302bce4a28d473
Leo Correa <lcorr005@gmail.com>
2016-05-08 11:51:47 -0400

    Make a bit more complex dummy

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -5,7 +5,29 @@ export default Ember.Controller.extend({
 
   actions: {
     testing() {
-      window.swal("Hello World", "success");
+      window.swal({
+        title: 'Submit email to run ajax request',
+        input: 'email',
+        showCancelButton: true,
+        confirmButtonText: 'Submit',
+        preConfirm: function() {
+          return new Ember.RSVP.Promise(function(resolve) {
+            window.swal.enableLoading();
+            setTimeout(function() {
+              resolve();
+            }, 2000);
+          });
+        },
+        allowOutsideClick: false
+      }).then(function(email) {
+        if (email) {
+          window.swal({
+            type: 'success',
+            title: 'Ajax request finished!',
+            html: 'Submitted email: ' + email
+          });
+        }
+      });
     },
     toggle() {
       this.set('toggleModal', true);
