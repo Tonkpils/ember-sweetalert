1c8d3a1024b77dedc7a7c8af39535043a82be04b
Leo Correa <lcorr005@gmail.com>
2016-05-08 23:38:29 -0400

    add es6 promise polyfill

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
 
     target.import(`${target.bowerDirectory}/sweetalert2/dist/sweetalert2.min.js`);
     target.import(`${target.bowerDirectory}/sweetalert2/dist/sweetalert2.min.css`);
+    target.import(`${target.bowerDirectory}/es6-promise-polyfill/promise.min.js`);
     // TODO: import IE support if configured to do so...
   }
 };
